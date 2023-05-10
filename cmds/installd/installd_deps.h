littleGnAl/mirror_platform_frameworks_native

Don't infer the default boot image location in otapreopt.
otapreopt calls derive_classpath but only takes BOOTCLASSPATH from the
output, not DEX2OATBOOTCLASSPATH or other env variables. The runtime
cannot handle this case when inferring the default boot image location
once aosp/2470962 is merged.

We don't have a boot image for otapreopt anyway, so just don't attempt
to infer the default boot image location.

Bug: 272573887
Test: ABTD (http://go/abtd-run/L42700000959054591): See no dex2oat
  crashes in the logs, though the test is failing due to another known
  issue (b/270940551)
Change-Id: I63c406b7f4c55247fec858d7b97234e7348a67da

cmds/installd/installd_deps.h
@@ -57,6 +57,9 @@ extern bool create_cache_path(char path[PKG_PATH_MAX],
                               const char *src,
                               const char *instruction_set);
 
+// If true, pass "--force-jit-zygote" to dex2oat (i.e., compile without a boot image).
+extern bool force_jit_zygote();
+
 }  // namespace installd
 }  // namespace android
 

mizdrake7/android_frameworks_native

cmds/installd/installd_deps.h
@@ -57,6 +57,9 @@ extern bool create_cache_path(char path[PKG_PATH_MAX],
                               const char *src,
                               const char *instruction_set);
 
+// If true, pass "--force-jit-zygote" to dex2oat (i.e., compile without a boot image).
+extern bool force_compile_without_image();
+
 }  // namespace installd
 }  // namespace android
 